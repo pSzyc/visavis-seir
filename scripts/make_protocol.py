5594da78b636babdad58f278db59c5f9ebca0623
ps <przemek.7678@gmail.com>
2023-09-11 12:18:48 +0200

    template moved to make_protocol.py

--- scripts/make_protocol.py
+++ scripts/make_protocol.py
@@ -2,9 +2,13 @@ import numpy as np
 from jinja2 import Template
 from pathlib import Path
 
-def make_protocol(pulse_intervals, duration, out_folder, template = './template.protocol'):
-    with open(template) as f:
-        template = Template(f.read())
+def make_protocol(pulse_intervals, duration, out_folder):
+    template = '''
+{% for t0, t1 in simulation_periods %}+batsoup 1
+run  {{t0}}s...{{t1}}s  [{{duration}}s]
+{% endfor %}
+'''
+    template = Template(template)
 
     pulse_times = list(np.cumsum(pulse_intervals))
     simulation_periods = list(zip([0] + pulse_times, pulse_times))
