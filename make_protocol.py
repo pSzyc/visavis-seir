2c2d06a7203ec64c143f39547c3fdbd25c5e5020
ps <przemek.7678@gmail.com>
2023-09-11 11:39:54 +0200

    Protocol function added

--- make_protocol.py
+++ make_protocol.py
@@ -1,21 +1,17 @@
 import numpy as np
-
 from jinja2 import Template
+from pathlib import Path
 
+def make_protocol(pulse_intervals, duration, out_folder, template = './template.protocol'):
+    with open(template) as f:
+        template = Template(f.read())
 
+    pulse_times = list(np.cumsum(pulse_intervals))
+    simulation_periods = list(zip([0] + pulse_times, pulse_times))
 
-with open('./template.protocol') as f:
-    template = Template(f.read())
-
-
-pulse_intervals = np.random.randint(80, 120, size=250)
-#pulse_intervals = np.arange(120, 80, -5)
-pulse_times = list(np.cumsum(pulse_intervals))
-
-simulation_periods = list(zip([0] + pulse_times, pulse_times))
-
-
-with open('./out.protocol', 'w') as f:
-    f.write(
-        template.render(simulation_periods=simulation_periods)
-    )
+    out_folder = Path(out_folder)
+    out_folder.mkdir(parents=True, exist_ok=True)
+    with open(out_folder / 'out.protocol', 'w') as f:
+        f.write(
+            template.render(simulation_periods=simulation_periods, duration=duration)
+        )
