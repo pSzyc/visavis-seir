e9c0c05ecec4f2cf048b6c433ef1f38d9e92aab3
Przemek <przemek.7678@gmail.com>
2023-10-27 07:45:33 +0200

    lambda.py integrated with visavis

--- scripts/lambda.py
+++ scripts/lambda.py
@@ -1,91 +1,58 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-from pathlib import Path
-data_path = Path("/home/hombresabio/AI/SpatkinModels/Simulate/Data")
-filename = "prop_estim"
-total_time = 2000
-h0 = 4
-h1 = 20
-h_every = 2
-n_sim = 500
-colnames=['time', 'F', 'A','S', 'E1', 'E2', 'I1', 'I2', "R1", "R2"]
+import csv
+from analyze_tracking import generate_dataset
+import click
+from multiprocessing import Pool
 
-nE = 5
-nI = 2
-nR = 4
+def get_results(df):
+    df_results = df.loc[:, ['ext', 'chaos']]
+    df_results['Position'] = df_results[['ext', 'chaos']].min(axis=1)
+    df_results['Event'] = df_results.idxmin(axis=1)
+    mean_steps = df_results['Position'].mean()
+    events = df_results['Event'].value_counts().reindex(['ext', 'chaos']).fillna(0).astype(int)
+    return events, mean_steps
 
-rE = 1.4
-rI = 1.4
-rR = 1/15
-r_act = 1
+def l_value(n_event, mean_steps, n, lenght, events):
+    l = n_event / (n_event * mean_steps + (n - n_event) * lenght)
+    l_chaos = events['chaos'] / n_event * l
+    l_ext = events['ext'] / n_event * l
+    return l, l_chaos, l_ext
 
-v = 1/(1 / (2 * r_act) + nE / rE) 
+def get_propensities(lenght, n_sim, width):
+    df = generate_dataset([], n_sim, channel_width=width, channel_length=lenght)
 
-def get_data(file):
-  print(file)
-  data = pd.read_csv(file / "Observables.dat", comment='#', header=0, names=colnames, delim_whitespace=True)
-  data = data[data['time'] >= 125]
-  data['R'] = data['R1'] + data['R2']
-  data['I'] = data['I1'] + data['I2']
-  data['E'] = data['E1'] + data['E2']
-  data.drop(['R1','R2','E1','E2','I1','I2'], axis=1, inplace=True)
-  return data
+    # Extinction
+    df['ext'] = np.nan
+    ext_mask = (df['fate'] == 'failure')
+    df.loc[ext_mask, 'ext'] = df.loc[ext_mask, 'track_end_position']
+    # Choas
+    df['chaos'] = df['significant_split_position']
 
-def get_one_stable(height):
-    return height * nR / rR * v 
+    events, mean_steps = get_results(df)
+    n_events = events.sum()
+    l, l_chaos, l_ext = l_value(n_events, mean_steps, n_sim, lenght, events)
+    return width, lenght, l, l_chaos, l_ext
 
-def ext_counter(data, total_time):
-    if len(data[data['R']==0]) > 0:
-        return data[data['R']==0].iloc[0]['time']
-    elif data['time'].max() < total_time:
-        return data[data['R']!=0].iloc[-1]['time']
-    else:
-        return np.nan
+def to_csv(results, result_file):
+    with open(result_file,'w') as out:
+        csv_out=csv.writer(out)
+        csv_out.writerow(['width', 'lenght', 'l', 'l_chaos', 'l_ext'])
+        for row in results:
+            csv_out.writerow(row)
 
-def chaos_counter(data, one_stable):
-    if len(data[data['R']>=one_stable * 1.6]) > 0:
-        return data[data['R']>= one_stable * 1.6].iloc[0]['time']
-    else:
-        return np.nan
-    
-def l_value(n_event, mean_t,n,T):
-    return n_event / (n_event * mean_t + (n - n_event) * T)
+@click.command()
+@click.argument('n_sim', type=int)
+@click.argument('w_start', type=int)
+@click.argument('w_end', type=int)
+@click.argument('results_file', type=str, default='lambda.csv')
+@click.argument('lenght', type=int, default=300)
+@click.argument('n_workers', type=int, default=5)
+def simulate(n_sim, w_start, w_end, results_file, lenght, n_workers):
+    results = []
 
+    with Pool(n_workers) as pool:
+            results = pool.starmap(get_propensities, [(lenght, n_sim, w)  for w in range(w_start, w_end)])  
+    to_csv(results, results_file)
 
-def get_propensities(height, n_sim = n_sim, total_time = total_time):
-    
-    data_list = []
-    for i in range(n_sim):
-        data_list.append(get_data(data_path / filename / f'out{height}-{i}'))
-    
-    treshold = get_one_stable(height)
-    T = total_time
-
-    series_chaos = pd.Series(chaos_counter(data_list[i], treshold) for i in range(n_sim))
-    series_ext = pd.Series(ext_counter(data_list[i], T) for i in range(n_sim))
-    df = pd.DataFrame({'Chaos': series_chaos, 'Ext': series_ext})
-    df['min'] = df.min(axis=1)  #.apply(lambda x: min(x['Chaos'], x['Ext']), axis=1)
-    mean_t = df['min'].mean()
-    l = l_value(len(df['min'].dropna()), mean_t, n_sim, T)
-    events = df.idxmin(axis=1).value_counts(dropna=True).reindex(['Chaos', 'Ext']).fillna(0)
-    l_ext = l * events['Ext'] / events.sum()
-    l_chaos = l * events['Chaos'] / events.sum()
-
-    return [l, l_ext, l_chaos, height]
-
-prop_list = []
-for height in range(h0, h1, h_every):
-    prop_list.append(get_propensities(height, n_sim))
-df = pd.DataFrame(prop_list, columns=['Lambda', 'Lambda Ext', 'Lambda Chaos', 'height'])
-
-df.set_index('height', inplace=True)
-df.plot()
-ax = plt.gca()
-
-plt.xlabel("Szerokość kanału (ilość komórek)")
-plt.ylabel("Propensity zdarzenia")
-plt.legend(['Porażka propagacji (Wygaśnięcie lub Chaos)', "Wygaśnięcie sygnału", "Wytworzenie się chaosu" ])
-plt.savefig("propensities_result.svg")
-#plt.savefig("propensities_result.png")
-
+if __name__ == '__main__':
+    simulate()
