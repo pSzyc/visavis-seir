1cba62ebc99f41fed7975315cbe3bcd5a1ded621
Przemek <przemek.7678@gmail.com>
2023-11-03 14:05:39 +0100

    two fronts interval, probabilites not propensities

--- figures/fig3/code/fig3B_get_data.py
+++ figures/fig3/code/fig3B_get_data.py
@@ -11,20 +11,14 @@ data_dir = Path(__file__).parent.parent / 'data'
 data_dir.mkdir(exist_ok=True, parents=True)
 
 channel_widths = [6]#[::-1]
-intervals = (list(range(100,300,100)))
+intervals = (list(range(60,300,100)))
 
 simulate(
-    n_sim=50,
+    n_sim=5,
     channel_widths=channel_widths,
-    results_file=data_dir / 'fig3B--propensities.csv',
+    results_file=data_dir / 'fig3B--probabilites.csv',
     channel_length=300,
     intervals=intervals,
     n_workers=1,
     interval_after=2660,
-    per_width_kwargs = {
-        w: {
-            'front_direction_minimal_distance': min(max(w - 1, 1), 5),
-            'min_peak_height': 0.03 / w,
-        } for w in channel_widths
-    }
     )
--- scripts/two_fronts.py
+++ scripts/two_fronts.py
@@ -2,54 +2,84 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 import numpy as np
-from scripts.analyze_tracking import generate_dataset
-from scripts.propensities import get_results, compute_propensities
+from scripts.defaults import PARAMETERS_DEFAULT
+from scripts.analyze_tracking import generate_dataset 
 from scripts.utils import starmap
-import csv
+import pandas as pd
+
+def get_pulse_fate_counts(
+        interval,
+        n_simulations,
+        parameters=PARAMETERS_DEFAULT,
+        channel_width=7,
+        channel_length=300,
+        duration=5,
+        interval_after=1500,
+        fate_criterion='reached',
+        offset=None,
+        n_margin=0,
+        outdir=None,
+        plot_results=False,
+        save_states=True,
+        save_iterations=True,
+        **kwargs,
+        ):
+
+    fields_to_groupby = (
+        ['fate', 'forward', 'backward'] if fate_criterion == 'generated' else 
+        ['fate', 'reached_end', 'reached_start'] if fate_criterion == 'reached' else
+        []
+    )
+    data = generate_dataset(
+        input_protocol=[interval],
+        n_simulations=n_simulations,
+        parameters=parameters,
+        channel_width=channel_width,
+        channel_length=channel_length,
+        duration=duration,
+        interval_after=interval_after,
+        offset=offset,
+        n_margin=n_margin,
+        outdir=outdir,
+        plot_results=plot_results,
+        save_states=save_states,
+        save_iterations=save_iterations,
+        **kwargs
+        )
+    data = data[data['pulse_id'] == 1]
+    data['interval'] = interval
+    counts = data.value_counts(['channel_length', 'channel_width', 'interval'] + fields_to_groupby).sort_index()
+    
+    counts = pd.DataFrame({'count': counts})
+    if outdir:
+        counts.to_csv(outdir / 'pulse_fate_count.csv')
+    return counts
+
+
 
 def get_propensities(interval, n_sim, channel_width, outdir=None, channel_length=300, **kwargs):
     print('----------', channel_width)
-    pulse_fates = generate_dataset([interval], n_simulations=n_sim, channel_width=channel_width, channel_length=channel_length, outdir=outdir, save_states=False, **kwargs)
-    pulse_fates = pulse_fates[pulse_fates['pulse_id'] == 1]
+    pulse_fates = get_pulse_fate_counts(interval, n_simulations=n_sim, channel_width=channel_width, channel_length=channel_length, outdir=outdir, save_states=False, **kwargs)
+    return pulse_fates
 
-    # Extintion position if failure
-    pulse_fates['failure'] = (
-        (pulse_fates['track_end_position'].fillna(1 if channel_width < 7 else np.nan)).where(pulse_fates['fate'] == 'lost_somewhere',
-        pulse_fates['track_end_position'].where(pulse_fates['fate'] == 'failure',
-        np.nan
-        ))) - 1 # minus one, as the simulation starts from h=1
-    
-    # Spawning position if spawning
-    pulse_fates['spawning'] = pulse_fates['significant_split_position'] - 1# minus one, as the simulation starts from h=1
-    event_counts, mean_steps = get_results(pulse_fates[['failure', 'spawning']])
-    any_event_count = event_counts.sum()
-    l, l_chaos, l_ext = compute_propensities(any_event_count, mean_steps, n_sim, channel_length - 1, event_counts) # minus one, as the simulation starts from h=1
-    return channel_width, channel_length, interval, l, l_chaos, l_ext
 
-def to_csv(results, result_file):
-    with open(result_file, 'w') as out:
-        csv_out=csv.writer(out)
-        csv_out.writerow(['channel_width', 'channel_length', 'interval', 'l', 'l_spawning', 'l_failure'])
-        for row in results:
-            csv_out.writerow(row)
 
 def simulate(n_sim, channel_widths, intervals, results_file, channel_length, n_workers, per_width_kwargs={}, **kwargs):
-    propensities = starmap(
+    counts = starmap(
         get_propensities,
         [
             dict(
                 channel_length=channel_length,
                 n_sim=n_sim,
                 interval=interval,
-                channel_width=w,
-                outdir=(results_file.parent / f'w-{w}-l-{channel_length}'/ f'interval-{interval}'),
-                
+                channel_width=w,                
                 ) | kwargs | (per_width_kwargs[w] if w in per_width_kwargs else {})
             for w in channel_widths for interval in intervals
         ],
         processes=n_workers,
         )
-    to_csv(propensities, results_file)
-
+    
+    data = pd.concat(counts)
+    data.to_csv(results_file)
 if __name__ == '__main__':
     simulate()
