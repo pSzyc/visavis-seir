2b1d61f8b4ecb9774e894a86d376270e83d51c79
pawnajo <pawnajo@szeptem.pl>
2023-11-10 13:13:49 +0100

    Add fig2C

--- figures/fig2/code/fig2C_get_data.py
+++ figures/fig2/code/fig2C_get_data.py
@@ -7,22 +7,22 @@ sys.path.insert(0, str(root_repo_dir)) # in order to be able to import from scri
 
 from scripts.propensities import simulate
 
-data_dir = Path(__file__).parent.parent / 'data2' / 'fig2C' / 'approach4'
+data_dir = Path(__file__).parent.parent / 'data' / 'fig2C' / 'approach6'
 data_dir.mkdir(exist_ok=True, parents=True)
 
 channel_widths = (list(range(1,10,1)) + list(range(10,21,2)))#[::-1]
 
 simulate(
-    n_sim=50,
+    n_sim=30000,
     channel_widths=channel_widths,
     results_file=data_dir / 'fig2C--propensities.csv',
-    channel_length=3000,
-    n_workers=1,
+    channel_length=300,
+    n_workers=20,
     interval_after=2660,
     per_width_kwargs = {
         w: {
             'front_direction_minimal_distance': min(max(w - 1, 1), 5),
             'min_peak_height': 0.03 / w,
         } for w in channel_widths
-    }
+    },
     )
--- figures/fig2/code/fig2C_plot.py
+++ figures/fig2/code/fig2C_plot.py
@@ -1,37 +1,52 @@
 from pathlib import Path
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator
+from sklearn.linear_model import LinearRegression
 from subplots_from_axsize import subplots_from_axsize
 
-data_dir = Path(__file__).parent.parent / 'data2' /'fig2C' / 'approach2'
+data_dir = Path(__file__).parent.parent / 'data' /'fig2C' / 'approach6'
 out_dir = Path(__file__).parent.parent / 'panels'
 
-data = pd.read_csv(data_dir / 'fig2C--propensities.csv').set_index('channel_width')
+propensities = pd.read_csv(data_dir / 'fig2C--propensities.csv')
 
-fig, ax = subplots_from_axsize(1, 1, (3,2.5))
 
-data.plot(y=['l', 'l_failure', 'l_spawning'], ax=ax)
-plt.legend([
-    'total event propensity ',
-    'failure propensity',
-    'spawning propensity',
-    ])
-plt.yscale('log')
+lreg = LinearRegression().fit(propensities[['channel_width']], propensities[['l_spawning']])
+a_sp, b_sp = lreg.coef_[0,0], lreg.intercept_[0]
 
-plt.savefig(out_dir / 'fig2C-log.png')
-plt.savefig(out_dir / 'fig2C-log.svg')
+propensities_cropped = propensities[propensities['channel_width'] <=6 ]
+lreg_failure = LinearRegression().fit(propensities_cropped[['channel_width']], np.log(propensities_cropped[['l_failure']]))
+a_fail, b_fail = lreg_failure.coef_[0,0], lreg_failure.intercept_[0]
 
-fig, ax = subplots_from_axsize(1, 1, (3,2.5))
 
-data.plot(y=['l', 'l_failure', 'l_spawning'], ax=ax)
-plt.legend([
-    'total ',
-    'propagation failure',
-    'additional front spawning',
-    ])
-plt.ylim(0,0.0005)
-plt.ylabel('propensity [steps$^{-1}$]')
-plt.xlabel('channel width') 
+def make_plot(ax):
+    xs = np.linspace(0,20,101)
+    ax.plot(propensities['channel_width'], propensities['l_failure'], 's', color='olive', label='failure')
+    ax.plot(propensities['channel_width'], propensities['l_spawning'], 'o', color='maroon',label='spawning')
+    ax.plot(xs, np.exp(lreg_failure.predict(xs.reshape(-1,1))), color='olive', alpha=0.3, label=f"$\\lambda_f$ = {np.exp(b_fail):.2f} $\\times$ {np.exp(-a_fail):.2f}$^{{-w}}$")
+    ax.plot(xs, lreg.predict(xs.reshape(-1,1)), color='maroon', alpha=.4, label=f"$\\lambda_s$ = (w - {-b_sp/a_sp:.2f}) / {1/a_sp:.0f}")
+    ax.plot(xs, np.exp(lreg_failure.predict(xs.reshape(-1,1))) + lreg.predict(xs.reshape(-1,1)), color='navy', alpha=.4, label=f"$\\lambda_f$ + $\\lambda_s$")
+    ax.set_xlabel('channel width')
+    ax.set_xlim(left=0)
+    ax.xaxis.set_major_locator(MultipleLocator(5))
+    ax.set_ylabel('propensity [step$^{-1}$]')
+
+
+# f"l_spawning = (w - {-b_sp/a_sp:.2f}) / {1/a_sp:.0f}"
+# f"l_failure =  1/({np.exp(-a_fail):.2f}^(w + {b_fail/a_fail:.3f})"
+
+fig, axs = subplots_from_axsize(1, 2, (3,3), left=1)
+
+make_plot(axs[0])
+axs[0].set_ylim(0,6e-4)
+axs[0].yaxis.set_major_locator(MultipleLocator(2e-4))
+axs[0].yaxis.set_major_formatter(lambda x,_: f"${x*10000:.0f} \\times 10^4$")
+
+make_plot(axs[1])
+axs[1].set_yscale('log')
+axs[1].set_ylim(2e-5, None)
+axs[1].legend()
 
 plt.savefig(out_dir / 'fig2C.png')
 plt.savefig(out_dir / 'fig2C.svg')
