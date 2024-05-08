49f7f0d1ea11fce68837f8b446c399d16227cf95
Marek Kochańczyk <mkochan@ippt.pan.pl>
2024-05-08 11:35:01 +0200

    Add qeirq as a submodule

--- scripts/compile_for_all_heights.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for width in {2..20}; do
-    sed -i "s/HEIGHT: usize = .*/HEIGHT: usize = $width;/" src/lattice.rs;
-    echo "compiling for length=$width"
-    cargo build --release;
-    echo "copying to ./target/bins/vis-a-vis-$width";
-    cp ./target/release/vis-a-vis ./target/bins/vis-a-vis-$width;
-done
