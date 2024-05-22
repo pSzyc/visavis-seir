eec9622443efa720b793ecff356b1e0286760368
pawnajo <pawnajo@szeptem.pl>
2024-05-22 17:16:55 +0200

    Update add_figures_to_git

--- scripts/add_figures_to_git.cmd
+++ scripts/add_figures_to_git.cmd
@@ -11,22 +11,38 @@ git add .\figures\fig2\panels\fig2E.png -f
 git add .\figures\fig3\fig3.png -f
 git add .\figures\fig3\panels\fig3A.png -f
 git add .\figures\fig3\panels\fig3B.png -f
+git add .\figures\fig3\panels\fig3CD.png -f
+git add .\figures\fig3\panels\fig3E.png -f
 git add .\figures\fig4\fig4.png -f
-git add .\figures\fig4\panels\fig4A.png -f
-git add .\figures\fig4\panels\fig4B.png -f
-git add .\figures\fig4\panels\fig4C.png -f
-git add .\figures\fig4\panels\fig4D.png -f
+git add .\figures\fig4\panels\fig4AB.png -f
+git add .\figures\fig4\panels\fig4CD.png -f
+git add .\figures\fig4\panels\fig4EF.png -f
+git add .\figures\fig4\panels\fig4GHI.png -f
+git add .\figures\fig5\fig5.png -f
+git add .\figures\fig5\panels\fig5A.png -f
+git add .\figures\fig5\panels\fig5B.png -f
+git add .\figures\fig5\panels\fig5C.png -f
+git add .\figures\fig6\fig6.png -f
+git add .\figures\fig6\panels\fig6A.png -f
+git add .\figures\fig6\panels\fig6BC.png -f
+git add .\figures\fig6\panels\fig6D.png -f
 git add .\figures\figS1\figS1.png -f
 git add .\figures\figS1\panels\figS1A.png -f
 git add .\figures\figS1\panels\figS1B.png -f
 git add .\figures\figS2\figS2.png -f
 git add .\figures\figS3\figS3.png -f
 git add .\figures\figS3\panels\figS3A.png -f
+git add .\figures\figS4\figS4.png -f
+git add .\figures\figS4\panels\figS4.png -f
+git add .\figures\figS5\figS5.png -f
+git add .\figures\figS5\panels\figS5.png -f
+git add .\figures\figS6\figS6.png -f
+git add .\figures\figS6\panels\figS6.png -f
 
 git add .\figures\fig1\fig1.svg -f
-git add .\figures\fig1\panels\fig1A.svg -f
-git add .\figures\fig1\panels\fig1B.svg -f
-git add .\figures\fig1\panels\fig1C.svg -f
+# git add .\figures\fig1\panels\fig1A.svg -f
+# git add .\figures\fig1\panels\fig1B.svg -f
+# git add .\figures\fig1\panels\fig1C.svg -f
 git add .\figures\fig2\fig2.svg -f
 git add .\figures\fig2\panels\fig2A.svg -f
 git add .\figures\fig2\panels\fig2B.svg -f
@@ -36,15 +52,30 @@ git add .\figures\fig2\panels\fig2E.svg -f
 git add .\figures\fig3\fig3.svg -f
 git add .\figures\fig3\panels\fig3A.svg -f
 git add .\figures\fig3\panels\fig3B.svg -f
+git add .\figures\fig3\panels\fig3CD.svg -f
+git add .\figures\fig3\panels\fig3E.svg -f
 git add .\figures\fig4\fig4.svg -f
-git add .\figures\fig4\panels\fig4A.svg -f
-git add .\figures\fig4\panels\fig4B.svg -f
-git add .\figures\fig4\panels\fig4C.svg -f
-git add .\figures\fig4\panels\fig4D.svg -f
+git add .\figures\fig4\panels\fig4AB.svg -f
+git add .\figures\fig4\panels\fig4CD.svg -f
+git add .\figures\fig4\panels\fig4EF.svg -f
+git add .\figures\fig4\panels\fig4GHI.svg -f
+git add .\figures\fig5\fig5.svg -f
+git add .\figures\fig5\panels\fig5A.svg -f
+git add .\figures\fig5\panels\fig5B.svg -f
+git add .\figures\fig5\panels\fig5C.svg -f
+git add .\figures\fig6\fig6.svg -f
+git add .\figures\fig6\panels\fig6A.svg -f
+git add .\figures\fig6\panels\fig6BC.svg -f
+git add .\figures\fig6\panels\fig6D.svg -f
 git add .\figures\figS1\figS1.svg -f
 git add .\figures\figS1\panels\figS1A.svg -f
 git add .\figures\figS1\panels\figS1B.svg -f
 git add .\figures\figS2\figS2.svg -f
 git add .\figures\figS3\figS3.svg -f
 git add .\figures\figS3\panels\figS3A.svg -f
-
+@REM git add .\figures\figS4\figS4.svg -f
+git add .\figures\figS4\panels\figS4.svg -f
+@REM git add .\figures\figS5\figS5.svg -f
+git add .\figures\figS5\panels\figS5.svg -f
+@REM git add .\figures\figS6\figS6.svg -f
+git add .\figures\figS6\panels\figS6.svg -f
