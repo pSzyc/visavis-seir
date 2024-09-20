5af45538663f015fd8c2494cfd84f26a1a018b8b
Marek Kochanczyk <mkochan@ippt.pan.pl>
2024-09-20 10:01:28 +0200

    Update defaults.py
    Use the default temporary folder

--- scripts/defaults.py
+++ scripts/defaults.py
@@ -14,8 +14,5 @@ PARAMETERS_DEFAULT = {
   "r_forward_rate": 0.0667,
 }
 
-
-# TEMP_DIR = f"/mnt/export/{os.environ['USER'] if 'USER' in os.environ else 'blabla'}/tmp" #f"/run/user/{os.environ['USER']}"
-TEMP_DIR = f"/run/user/{os.environ['USER'] if 'USER' in os.environ else 'blabla'}"
-# TEMP_DIR = "/tmp"
-
+#TEMP_DIR = f"/run/user/{os.getuid()}"
+TEMP_DIR = "/tmp"
