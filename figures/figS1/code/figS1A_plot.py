9a1aaa5bc09478244f66f96f79b4dbc2e3bcd7a3
pawnajo <pawnajo@szeptem.pl>
2025-01-23 13:56:12 +0100

    Update fig S1 to plot 1/v instead of v

--- figures/figS1/code/figS1A_plot.py
+++ figures/figS1/code/figS1A_plot.py
@@ -15,22 +15,24 @@ data_dir = Path(__file__).parent.parent.parent.parent / 'data' / 'figS1' / "appr
 panel_dir = Path(__file__).parent.parent / 'panels'
 panel_dir.mkdir(exist_ok=True, parents=True)
 
-fig, ax = subplots_from_axsize(1, 1, (2.5, 2), left=.8)
+fig, ax = subplots_from_axsize(1, 1, (2.5, 2), left=1)
 
 velocity = pd.read_csv(data_dir / 'velocity.csv').set_index('channel_width')
-velocity[velocity['channel_length'].eq(300)].plot(style='o', lw=0, ax=ax, color="C4")
-velocity[velocity['channel_length'].eq(30) & (velocity.index.get_level_values('channel_width') == 1)].plot(style='o', fillstyle='none', ax=ax, color="C4")
+velocity[velocity['channel_length'].eq(300)]['velocity'].pipe(lambda x: 1/x).plot(style='o', lw=0, ax=ax, color="C4")
+velocity[velocity['channel_length'].eq(30) & (velocity.index.get_level_values('channel_width') == 1)]['velocity'].pipe(lambda x: 1/x).plot(style='o', fillstyle='none', ax=ax, color="C4")
 
-yticks = [0.1 * y for y in range(5)]
-ax.set_ylabel('front propagation speed $v$ [cell layer/min]')
+ax.set_ylabel('average inverse front propagation speed\n$v^{-1}$ [min / cell layer]')
 ax.set_xlabel('channel width $W$')
 ax.set_xlim(left=0)
-ax.set_ylim(bottom=0, top = 0.4)
+ax.set_ylim(bottom=0, top = 6)
 ax.xaxis.set_major_locator(MultipleLocator(5))
-ax.yaxis.set_ticks(yticks + [1/4.5, 1/3.5], [f"{x:.1f}" for x in yticks] + [r'$v_{\mathrm{deterministic}}$', r'$v_{\mathrm{asymptotic}}$'])
-ax.hlines([1/4.5], 0, 22, linestyles=':', color='gray')
-ax.hlines([1/3.5], 0, 22, linestyles='--', color='gray')
+ax.yaxis.set_major_locator(MultipleLocator(1))
+ax.yaxis.set_minor_locator(MultipleLocator(.5))
+ax.hlines([4.5], 0, 22, linestyles=':', color='gray')
+ax.hlines([3.5], 0, 22, linestyles='--', color='gray')
+yticks = ax.get_yticks()
+ax.yaxis.set_ticks(list(yticks) + [4.5, 3.5], [f"{x:.0f}" for x in yticks] + [r'$v^{-1}_{\mathrm{deterministic}}$', r'$v^{-1}_{\mathrm{asymptotic}}$'])
+
 ax.grid(which='both', ls=':', alpha=.4)
-ax.get_legend().set_visible(False)
 plt.savefig(panel_dir / 'figS1A.png')
 plt.savefig(panel_dir / 'figS1A.svg')
