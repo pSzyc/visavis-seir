d68dd921bcfab9353b5590513a34a18008b3036a
pawnajo <pawnajo@szeptem.pl>
2025-01-27 12:39:32 +0100

    Change licence to BSD 3-Clause

--- figures/fig3/code/fig3A_get_data.py
+++ figures/fig3/code/fig3A_get_data.py
@@ -1,6 +1,6 @@
 # Script used in Information transmission in a cell monolayer: A numerical study, Nałęcz-Jawecki et al. 2024
-# Copyright (C) 2024 Paweł Nałęcz-Jawecki, Przemysław Szyc, Frederic Grabowski, Marek Kochańczyk
-# Available under GPLv3 licence, see README for more details 
+# Copyright (C) 2024-2025 Paweł Nałęcz-Jawecki, Przemysław Szyc, Frederic Grabowski, Marek Kochańczyk
+ 
 
 from pathlib import Path
 from shutil import rmtree
